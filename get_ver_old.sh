4d9104ff99c07411954c734c5f2e81efd463d540
Cronomantic <cronomantic@gmail.com>
2026-07-03 10:03:22 +0200

    Higiene del repositorio: scripts _old, docs de desarrollo y .gitignore
    - Eliminados 5 scripts *_old superados por los actuales.
    - Movidas 6 notas de desarrollo (refactors ya completados) a doc/dev/.
    - Ampliado .gitignore: artefactos de PLY, salida de compilacion, ZIPs de
      release, emulador local y ficheros de prueba de la raiz.

--- get_ver_old.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-OUTPUT="$(git describe --abbrev=0 --tags)"
-OUTPUT=${OUTPUT//./_}
-CDATE="$(date +%Y_%m_%d)"
-echo "${OUTPUT}"_"${CDATE}" > ./version.txt
--- make_dist_old.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-./get_ver.sh
-python make_dist.py
